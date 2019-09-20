d5cee26db26f0382ef8847e2958d6fb6a9d6fba4
Kostas Minaidis <minaidisk@gmail.com>
2019-09-20 10:53:52 +0300

    [ STEP 13 ] Adding an Event Listener that will be triggered each time the cell value changes

--- excel.js
+++ excel.js
@@ -6,5 +6,15 @@ console.log("Running");
 let unitCol = document.querySelector("#units");
 console.log( unitCol );
 
+// Adding an Event Listener on our Cell:
+// Syntax: HTMLElement.addEventListener( EVENT:String, CALLBACK:Function );
+unitCol.addEventListener( "input", handleClick );
+
+function handleClick( e ){
+
+    console.log( "The Input has been changed!" );
+
+}
+
 console.log("End of Code");
 // END OF CODE
