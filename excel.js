6e1a6c911fc7645d6847ca50b890cea0bb6c6232
Kostas Minaidis <minaidisk@gmail.com>
2019-09-20 10:51:39 +0300

    [ STEP 12 ] Cleaning up demo code

--- excel.js
+++ excel.js
@@ -5,25 +5,6 @@ console.log("Running");
 
 let unitCol = document.querySelector("#units");
 console.log( unitCol );
-// Change Style:
-// CSS: background-color: red;
-unitCol.style.backgroundColor = "red";
-// CSS: border: 2px solid black;
-unitCol.style.border = "2px solid black";
-// Change Content:
-unitCol.textContent = "595959";
-
-// run();
-
-try {
-
-    run();
-
-} catch(error) {
-
-    console.log( error, error.message );
-
-}
 
 console.log("End of Code");
 // END OF CODE
